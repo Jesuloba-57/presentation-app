2e96caef8ffc026e7a563928dcf254a77a70afb8
Jesuloba David <Jesulobadavid@gmail.com>
2026-03-19 13:37:42 -0400

    mobile dashboard fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export default function Navbar() {
                             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
                             <span className="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
                         </span>
-                        Live Stage
+                        Live
                     </span>
                 </NavLink>
 
